fbd6eee9d67f983bd7a20f0fab7fedfe75b556bf
Titanium Brain <titaniumbrain@vivaldi.net>
2022-10-20 20:39:17 +0100

    docs(package): add package __init__ docstring

--- src/regex_gen/__init__.py
+++ src/regex_gen/__init__.py
@@ -1,3 +1,9 @@
+"""
+A simple, object oriented, regular expression generator.
+"""
+
 # read version from installed package
 from importlib.metadata import version
+
 __version__ = version("regex_gen")
+
