99e5e6a093579bbe849d98c5b1c9cff685e1b655
Titanium Brain <titaniumbrain@vivaldi.net>
2022-11-09 15:12:47 +0000

    feat(__init__): expose names to top-level
    Imports names from regex_gen.regex_gen into package __init__

--- src/regex_gen/__init__.py
+++ src/regex_gen/__init__.py
@@ -7,3 +7,35 @@ from importlib.metadata import version
 
 __version__ = version("regex_gen")
 
+# export names from inner module
+from .regex_gen import (
+    ASCII,
+    DOTALL,
+    IGNORECASE,
+    LOCALE,
+    MULTILINE,
+    VERBOSE,
+    A,
+    AnyOf,
+    Comment,
+    Flag,
+    Flags,
+    Group,
+    I,
+    If,
+    L,
+    M,
+    Negative_LookAhead,
+    Negative_LookBehind,
+    NoneOf,
+    OneOrMore,
+    Optional,
+    Or,
+    Positive_LookAhead,
+    Positive_LookBehind,
+    Regex,
+    Repeat,
+    S,
+    X,
+    ZeroOrMore,
+)
